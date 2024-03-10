96a48bc985a8f23e981abf2f900330fadf06f64e
root <root@node12.cluster.local>
2024-03-10 11:14:40 +0800

    240310

---
+++ ov_segment/fc-clip/test.py
@@ -0,0 +1,4 @@
+import torch
+data1 = torch.load("/root/wangfeiyue3new/sby/fc-clip/superpixel/0text_embedding.pt")
+path1 = torch.load("/root/wangfeiyue3new/sby/fc-clip/superpixel/1text_embedding.pt")
+import pdb; pdb.set_trace()
